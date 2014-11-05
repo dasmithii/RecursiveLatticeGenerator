5c9225ad730fb1fd3f4cd6bc45419a1656f8dd4c
Adam Smith <iihtimsad@gmail.com>
2014-11-04 23:26:51 -0500

    setup docopt

--- main.py
+++ main.py
@@ -1,57 +1,25 @@
+"""Recursive Lattice Generator
+
+Usage:
+  main.py [--dimension=<d>] ([--output=<f>] | [--count=<c>] [--threads=<ts>])
+  main.py (-h | --help)
+  main.py --version
+
+Options:
+  -h --help         Show this screen.
+  --version         Show version.
+  --dimension=<d>   Specify lattice dimension.   [default: 3]
+  --output=<file>   Specify output directory.    [default: ./dump]
+  --count=<c>       Specify lattice count.       [default: 1]
+  --threads=<ts>    Limit number of threads.     [default: 1]
+
+
+"""
+from docopt import docopt
 from pyprocessing import *
-from explorer import Explorer
-import sys
 
-
-# Command line help.
-usage = ''' - usage:
-       =>  program [lattice-dimension]'''
-
-
-# Global explorer.
-explorer = Explorer()
-
-
-# Prepares drawing window.
-def setup():
-	size(screen.width, screen.height)
-	rectMode(CENTER)
-	explorer.prepare()
-
-
-
-# Update for next frame.
-def draw():
-	background(0)
-	explorer.draw()
-
-
-# Helper for input errors.
-def invalid(message):
-	print ' - ERROR: ' + message + '.'
-	print usage
-	exit(1)
-
-
-# React to command line arguments and start the lattice
-# explorer.
-if __name__ == "__main__":
-	args = sys.argv[1:]
-	if len(args) > 1:
-		invalid('too many arguments')
-	elif len(args) == 1:
-		if args[0] == 'help':
-			print usage
-			exit(0)
-		try:
-			prongs = int(args[0])
-		except:
-			invalid('dimension not parsable')
-		if prongs <= 1:
-			invalid('dimension <= 1')
-		explorer.set_dimension(prongs)
-	run()
-
-
-
-
+# Parse command line arguments via docopt and start the
+# lattice generator.
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='2.0')
+    print(arguments)
