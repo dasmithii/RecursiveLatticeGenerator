e477f28e5be95d1b604cc5cf67cc7322b7170c19
Adam Smith <iihtimsad@gmail.com>
2014-11-04 23:34:16 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 """Recursive Lattice Generator
 
 Usage:
-  main.py [--dimension=<d>] ([--output=<f>] | [--count=<c>] [--threads=<ts>])
+  main.py [--dimension=<>] ([--output=<>] | [--count=<>] [--threads=<>])
   main.py (-h | --help)
   main.py --version
 
 Options:
-  -h --help         Show this screen.
-  --version         Show version.
-  --dimension=<d>   Specify lattice dimension.   [default: 3]
-  --output=<file>   Specify output directory.    [default: ./dump]
-  --count=<c>       Specify lattice count.       [default: 1]
-  --threads=<ts>    Limit number of threads.     [default: 1]
+  -h --help          Show this screen.
+  --version          Show version.
+  --count=<n>         Specify lattice count.
+  --dimension=<n>     Specify lattice dimension.   [default: 3]
+  --output=<file>    Specify output directory.    [default: ./dump]
+  --threads=<n>      Limit number of threads.     [default: 1]
 
 
 """
 from docopt import docopt
 from pyprocessing import *
 
+
 # Parse command line arguments via docopt and start the
 # lattice generator.
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='2.0')
-    print(arguments)
+	args = docopt(__doc__, version='2.0')
+	print args
